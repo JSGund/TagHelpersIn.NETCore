aff7518bd756a73c1ff131fd49e8cfc55a794a71
Jeetendra Gund <jsgund@gmail.com>
2020-05-02 16:33:31 +0530

    Image folder added

---
+++ TagHelpersIn.NETCore/TagHelpersIn.NETCore/Models/LanguageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagHelpersIn.NETCore.Models
+{
+    public class LanguageViewModel
+    {
+        public string Language { get; set; }
+
+        public List<SelectListItem> Languages { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "CS", Text = "C#" },
+            new SelectListItem { Value = "JS", Text = "JavaScript" },
+            new SelectListItem { Value = "TS", Text = "TypeScript"  },
+        };
+    }
+}
